92852cf204a7554b28268b6c666f6ea3543b254e
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-02-15 14:44:36 +0100

    Update ColorPicker Component & styles.css

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -3,11 +3,24 @@ import { useState } from 'react';
 export default function ColorPicker() {
     const [color, setColor] = useState("#FFFFFF");
 
+    function handleColorChange (e) {
+        const color = e.target.value;
+        const wrapper = document.querySelector('.wrapper');
+
+        setColor(color);
+        wrapper.style.backgroundColor = color;
+    }
+
     return (
         <div className="wrapper">
-            <h1>Color picker</h1>
-            <p>Selected Color</p>
-            <h2>{color}</h2>
+            <div className="container">
+                <h1>Color picker</h1>
+                <p>Selected Color</p>
+                <h2>{color}</h2>
+
+                <h1>Pick a color</h1>
+                <input type="color" value={color} onChange={handleColorChange} />
+            </div>
         </div>
     );
 }
