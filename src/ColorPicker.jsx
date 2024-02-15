ea40735c05fffcf7cc4359914e0e547cc44c9b68
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-02-15 14:28:14 +0100

    Update ColorPicker.jsx

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,5 +1,13 @@
 import { userState } from 'react';
 
 export default function ColorPicker() {
-    
+    const [color, setColor] = userState("#FFFFFF");
+
+    return (
+        <div className="wrapper">
+            <h1>Color picker</h1>
+            <p>Selected Color</p>
+            <h2>{color}</h2>
+        </div>
+    );
 }
